8d11d87b4889610d9b191c57ed38c13f318fa507
ahmed-bhatti <ahmedalirajpootbhatti@gamil.com>
2022-12-10 15:17:13 -0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: '1rem',
+
+      // default breakpoints but with 40px removed
+    screens: {
+      sm: '600px',
+      md: '728px',
+      lg: '984px',
+      xl: '1140px',
+    },
+  },
+},
+  plugins: [],
+} 
+
